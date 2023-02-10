8e500075ae90d886f4f470403694788f70c1c92d
JonathanHaws <jonathanmhaws1@gmail.com>
2023-02-10 12:06:03 -0700

    added styling to the card

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 
 function createCard(name, role, id, email, role_specific_info){
-    let header = '<h1>' + name + '</h1>\n';
-    header += '<h2>'+ role + '</h2>\n';
-    let info ='<h3>'+ id + '</h3>\n';
-    info += '<h3>'+ email + '</h3>\n';
-    info += '<h3>'+ role_specific_info + '</h3>\n';
-    return '<div class="card"><div>'+ header+ '</div><div>'+ info +'</div></div>';
+    let textstyle = 'color:#d1d1d1;'
+    let header = '<h1 style="'+textstyle+'">' + name + '</h1>';
+    header += '<h2 style="'+textstyle+'">'+ role + '</h2>';
+    let info ='<h3 style="'+textstyle+'">'+ id + '</h3>';
+    info += '<h3 style="'+textstyle+'">'+ email + '</h3>';
+    info += '<h3 style="'+textstyle+'">'+ role_specific_info + '</h3>';
+    let cardstyle = 'width:min-content; margin:auto; '
+    let headerstyle = 'background-color:#9c7535; padding:15px; border-radius:12px 12px 0px 0px;';
+    let infostyle = 'background-color:#ba8c41; padding:15px; border-radius:0px 0px 12px 12px;'
+    return '<div style="'+ cardstyle +'"><div style="'+ headerstyle +'">'+ header +'</div><div style ="' + infostyle + '">' + info + '</div></div>\n';
 }
 
 inquirer.prompt([
@@ -20,13 +24,14 @@ inquirer.prompt([
     { type: 'input', message: 'What is the managers office id?', name: 'office_id',}
 
     ]).then(function(response){
+        
         let manager = new Manager(response.name, response.id, response.email, response.office_id);
         console.log(manager.getRole());
 
-        let htmldoccument = createCard(manager.getName(),manager.getRole(),manager.getId(),manager.getEmail(),manager.getOfficeNumber());
-        let cssdocument = '\n.card{\n  background-color:coral;  background-color:#d6b65c;\n  padding:15px;\n  width:min-content;\n}\n '
-        
+        let htmldoccument; let cssdocument;
+        htmldoccument = createCard(manager.getName(),manager.getRole(),manager.getId(),manager.getEmail(),manager.getOfficeNumber());
         htmldocument = '\n<!DOCTYPE html>\n  <head>\n    <link rel="stylesheet" href="style.css">\n  </head>\n  <body>\n    ' + htmldoccument + '  </body>\n</html>\n';
+        cssdocument = '\nbody{\n background-color:#d1d1d1;\n text-align:center;\n}\n'
         fs.writeFile('./dist/index.html', htmldocument, (err)=>{})
         fs.writeFile('./dist/style.css', cssdocument , (err)=>{});
     });
