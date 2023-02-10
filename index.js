5366caeb1174a7e460d62c337349dda3fe749072
JonathanHaws <jonathanmhaws1@gmail.com>
2023-02-10 10:41:04 -0700

    dynamic html

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 
-fs.writeFile('./dist/index.html', 'test', (err)=>{})
-fs.writeFile('./dist/style.css', 'test', (err)=>{});
-
-var employees = [];
-
 inquirer.prompt([
     { type: 'input', message: 'What is the managers name?', name: 'name',},
     { type: 'input', message: 'What is the managers id?', name: 'id',},
@@ -19,6 +14,11 @@ inquirer.prompt([
         let manager = new Manager(response.name, response.id, response.email, response.office_id);
         console.log(manager.getRole());
 
+        let htmlcontent = '<div> <p> Manager </p>' + manager.getName() + '</div>';
+        let htmldocument = '<!DOCTYPE html>\n  <head>\n    <link rel="stylesheet" href="style.css">\n  </head>\n  <body>\n    ' + htmlcontent + '  </body>\n</html>';
+
+        fs.writeFile('./dist/index.html', htmldocument, (err)=>{})
+        fs.writeFile('./dist/style.css', '', (err)=>{});
     });
 
 //{ type: 'list', message: 'What type of employee?', name: 'type', choices: ['Engineer', 'Intern',],}])
