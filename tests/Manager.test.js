52995a343b9070ddcf751899ac70688a77283972
JonathanHaws <jonathanmhaws1@gmail.com>
2023-02-12 04:48:43 -0700

    example

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -4,5 +4,5 @@ const Manager = require('./../lib/Manager.js');
 test('Manager module not returning correct values for Role and Office Number',()=>{ 
     let manager = new Manager('Jonathan',3,'jonathan@email.com','Number 4')
     expect(manager.getRole()).toBe("Manager")
-    expect(manager.getOfficeNumber()).toBe("Number 3")    
+    expect(manager.getOfficeNumber()).toBe("Number 4")    
 });
